3d79b58f1aff87e23816ace9fcb3c7dbde09cb55
Drew Vollmer <drew.vollmer@gmail.com>
2019-10-15 18:01:54 -0400

    rsvp tests

--- append_to_csv.php
+++ append_to_csv.php
@@ -1,14 +1,15 @@
 <?php
-    $keys = array('names','attending','rehearsal_dinner','dietary_restrictions');
-    $csv_line = array();
-    foreach($keys as $key){
-        array_push($csv_line,'' . $_GET[$key]);
-    }
-    $fname = 'rsvp_list.csv';
-    $csv_line = implode(',',$csv_line);
-    if(!file_exists($fname)){$csv_line = "\r\n" . $csv_line;}
-    $fcon = fopen($fname,'a');
-    $fcontent = $csv_line;
-    fwrite($fcon,$csv_line);
-    fclose($fcon);
+$fieldA = $_POST["textnames"];
+$keys = array('names','attending','rehearsal_dinner','dietary_restrictions'); //THIS IS WHERE YOU PUT THE FORM ELEMENTS ex: array('$fieldA','$fieldB',etc)
+$csv_line = array();
+foreach($keys as $key){
+    array_push($csv_line,'' . $_GET[$key]);
+}
+$fname = 'rsvp_list.csv';
+$csv_line = implode(',',$csv_line);
+if(!file_exists($fname)){$csv_line = "\r\n" . $csv_line;}
+$fcon = fopen($fname,'a');
+$fcontent = $csv_line;
+fwrite($fcon,$csv_line);
+fclose($fcon);
 ?>
